079675b8c58285fd52706a1614cecbb0ac7a2093
edenpd <eden1998p@gmail.com>
2021-03-16 21:43:38 +0200

    Initial commit of events page

--- Client/SolMate/routes/EventsRoute.tsx
+++ Client/SolMate/routes/EventsRoute.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const EventsRoute = () => {
     return (
         <View>
-            <Text>Events Route</Text>
+            <Text>Events Page</Text>
         </View>
     );
 }
