797f346e61f666bbdeb6e06d408d77d0ad6e3bbf
adibigler <adibigler96@gmail.com>
2021-03-24 16:53:54 +0200

    AA

--- Client/SolMate/App.js
+++ Client/SolMate/App.js
@@ -1,41 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { BottomNavigation, Appbar } from 'react-native-paper';
-import ChatRoute from './routes/ChatRoute';
-import EventsRoute from './routes/EventsRoute';
-import ProfileRoute from './routes/ProfileRoute';
-import MatchesRoute from './routes/MatchesRoute';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BottomNavigation, Appbar } from "react-native-paper";
+import ChatRoute from "./routes/ChatRoute";
+import EventsRoute from "./routes/EventsRoute";
+import ProfileRoute from "./routes/ProfileRoute";
+import MatchesRoute from "./routes/MatchesRoute";
 import Header from "./components/Header";
+import SettingRoute from "./routes/SettingsRoute";
 
 export default function App() {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'matches', title: 'Matches', icon: 'account-multiple' },
-    { key: 'events', title: 'Events', icon: 'calendar-blank' },
-    { key: 'chat', title: 'Chat', icon: 'chat' },
-    { key: 'profile', title: 'Profile', icon: 'account' }
+    { key: "matches", title: "Matches", icon: "account-multiple" },
+    { key: "events", title: "Events", icon: "calendar-blank" },
+    { key: "chat", title: "Chat", icon: "chat" },
+    { key: "profile", title: "Profile", icon: "account" },
+    { key: "setting", title: "Setting", icon: "cog" },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     matches: MatchesRoute,
     events: EventsRoute,
     chat: ChatRoute,
-    profile: ProfileRoute
+    profile: ProfileRoute,
+    setting: SettingRoute,
   });
-  
+
   const navigationStyle = {
-    backgroundColor: 'white'
-  }
+    backgroundColor: "white",
+  };
 
   return (
-    <View style={{
-      width: '100%',
-      height: '100%'
-    }}>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <Header />
       <BottomNavigation
-        activeColor={'purple'}
+        activeColor={"purple"}
         barStyle={navigationStyle}
         navigationState={{ index, routes }}
         onIndexChange={setIndex}
---
+++ Client/SolMate/routes/SettingsRoute.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Avatar, TextInput } from "react-native-paper";
+import {
+  ActionsContainer,
+  Button,
+  FieldsContainer,
+  Fieldset,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Switch,
+} from "react-native-clean-form";
+const settings = StyleSheet.create({
+  userImage: {
+    alignSelf: "center",
+  },
+  Title: {
+    alignItems: "center",
+    alignContent: "center",
+    color: "purple",
+    textAlign: "center",
+    alignSelf: "center",
+    fontSize: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+  row: {
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "row",
+  },
+  inputWrap: {
+    flex: 1,
+    borderColor: "#cccccc",
+    borderBottomWidth: 1,
+    marginBottom: 10,
+  },
+  label: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+});
+
+const SettingRoute = () => {
+  return (
+    <View>
+      <Avatar.Image
+        style={settings.userImage}
+        source={{ uri: "https://picsum.photos/700" }}
+      />
+      <Text style={settings.Title}>Adi Bigler</Text>
+      <FieldsContainer>
+        <Fieldset label="Contact details">
+          <FormGroup>
+            <Label>First name</Label>
+            <Input placeholder="Esben" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input placeholder="esbenspetersen@gmail.com" />
+          </FormGroup>
+        </Fieldset>
+        <Fieldset label="Password" last>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input placeholder="Enter a password" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Repeat password</Label>
+            <Input placeholder="Repeat your password" />
+          </FormGroup>
+          <FormGroup border={false}>
+            <Label>Save my password</Label>
+            <Switch />
+          </FormGroup>
+        </Fieldset>
+      </FieldsContainer>
+    </View>
+  );
+};
+
+export default SettingRoute;
