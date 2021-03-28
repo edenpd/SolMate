f06b46dff9f83a0b71ea4ba0f95e8aa1062e9764
edenpd <eden1998p@gmail.com>
2021-03-28 16:57:14 +0300

    Connectio to seatgeek - server & client

--- Server/src/controllers/eventController.ts
+++ Server/src/controllers/eventController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { CallbackError } from "mongoose";
 import Event, { IEvent } from "../modules/eventModel";
+import axios from "axios";
+
+const client_id = "MjE2NDYzNTF8MTYxNjkxOTA0OS4yODI1MjUz";
 
 // const Songkick = require('songkick-api-node');
 // const songkickApi = new Songkick('YourApiKey');
-// const axios = require('axios');
 
 export const addEvent = async (req: Request, res: Response) => {
   // try {
@@ -42,40 +44,92 @@ export const getEvents = async (req: Request, res: Response) => {
 
   console.log("Getting events");
 
-  const events: IEvent[] = [
-    {
-      EventId: 11129128,
-      EventName: "aaaaaaaaaaaaa",
-      StartDateTime: '2012-04-18T20:00:00-0800',
-      ArtistName: 'Wild Flag',
-      CityName: 'San Francisco, CA, US',
-      VenueName: 'The Fillmore',
-      EventUrl: 'http://www.songkick.com/concerts/11129128-wild-flag-at-fillmore?utm_source=PARTNER_ID&utm_medium=partner'
-    },
-    
-  ];
+  if (req.query.artists == undefined) {
+    res.status(500).send("Error");
+    return;
+  }
 
-  res.status(200).json(events);
+  const artists: string[] = req.query.artists.toString().split(",");
+  let suggestionsArtistsUrl = "";
+  let events: IEvent[] = [];
 
-  // axios.get('https://app.ticketmaster.com/discovery/v2/events?apikey=ScXzHTbNBRcdHd875uxjKAKUIbF0d9lI&locale=*&page=14')
-  //   .then(function (response: any) {
-  //     // handle success
-  //     console.log("res:" + response);
-  //     res.status(200).json(response);
+  //  Gets the artists id
+  for (let artist of artists) {
+    await axios.get('https://api.seatgeek.com/2/performers?q=' + artist + '&client_id=' + client_id)
+      .then(async function (response: any) {
+        // console.log(artist + ":" + response.data.performers[0].id);
+        suggestionsArtistsUrl += "&performers.id=" + response.data.performers[0].id;
 
-  //   })
-  //   .catch(function (error: any) {
-  //     // handle error
-  //     res.status(500).send(error);
-  //   })
+        // Gets the artist's first 5 events
+        await axios.get('https://api.seatgeek.com/2/events?performers.id=' + response.data.performers[0].id + '&per_page=5&client_id=' + client_id)
+          .then(function (response: any) {
 
-  // await Event.find(
-  //   { sender: req.body.sender },
-  //   (err: CallbackError, event: IEvent[]) => {
-  //     if (err) {
-  //     } else {
-  //       res.status(200).json(event);
-  //     }
-  //   }
-  // );
+            response.data.events.forEach(function (event: any) {
+
+              let artistsNames = "";
+              event.performers.forEach(function (performer: any) {
+                artistsNames += performer.name + ", ";
+              });
+      
+              artistsNames = artistsNames.substring(0, artistsNames.length - 2);      
+
+              let newEvent: IEvent = {
+                EventId: event.id,
+                EventName: event.short_title,
+                StartDateTime: event.datetime_local,
+                ArtistName: artistsNames,
+                CityName: event.venue.extended_address,
+                VenueName: event.venue.name,
+                EventUrl: event.url,
+                Image: event.performers[0].image,
+                IsRecommended: false
+              }
+              events.push(newEvent);
+            });
+
+          })
+          .catch(function (error: any) {
+            console.log("Not found events for " + artist);
+          })
+
+      })
+      .catch(function (error: any) {
+        console.log("Artist " + artist + "not Found");
+      })
+  }
+
+  // Gets the recommended events
+  await axios.get('https://api.seatgeek.com/2/recommendations?' + suggestionsArtistsUrl + '&postal_code=10014&per_page=10&client_id=' + client_id)
+    .then(function (response: any) {
+
+      response.data.recommendations.forEach(function (recommendation: any) {
+
+        let artistsNames = "";
+        recommendation.event.performers.forEach(function (performer: any) {
+          artistsNames += performer.name + ", ";
+        });
+
+        artistsNames = artistsNames.substring(0, artistsNames.length - 2);
+
+        let newEvent: IEvent = {
+          EventId: recommendation.event.id,
+          EventName: recommendation.event.short_title,
+          StartDateTime: recommendation.event.datetime_local,
+          ArtistName: artistsNames,
+          CityName: recommendation.event.venue.extended_address,
+          VenueName: recommendation.event.venue.name,
+          EventUrl: recommendation.event.url,
+          Image: recommendation.event.performers[0].image,
+          IsRecommended: true
+        }
+        events.push(newEvent);
+      });
+
+    })
+    .catch(function (error: any) {
+      console.log("ERROR with suggested events");
+    })
+
+    res.status(200).json(events);
+
 };
