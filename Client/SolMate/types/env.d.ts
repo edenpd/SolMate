124b41206d89171e5f67f9c4a35174f4fe8f8320
adibigler <adibigler96@gmail.com>
2021-04-16 19:43:19 +0300

    fix algo 2

--- Client/SolMate/types/env.d.ts
+++ Client/SolMate/types/env.d.ts
@@ -1,9 +1,9 @@
-declare module '@env' {
-    export const API_BASE: string;
-    export const SERVER_ADDRESS: string;
-    export const SERVER_PORT: string;
-    export const EXPO_ADDRESS: string;
-    export const EXPO_PORT: string;
-    export const CHAT_SOCKET_ADDRESS: string;
-    export const CHAT_SOCKET_PORT: string;
+declare module "@env" {
+  export const API_BASE: string;
+  export const SERVER_ADDRESS: string;
+  export const SERVER_PORT: string;
+  export const EXPO_ADDRESS: string;
+  export const EXPO_PORT: string;
+  export const CHAT_SOCKET_ADDRESS: string;
+  export const CHAT_SOCKET_PORT: string;
 }
