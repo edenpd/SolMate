5894de4f4d6960f102482c9b6ea727147d8d9548
Shiri Mesika <shirimes@gmail.com>
2021-04-17 00:18:53 +0300

    get user data from db

--- Client/SolMate/routes/ProfileRoute.tsx
+++ Client/SolMate/routes/ProfileRoute.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React ,{ useState, useEffect, useContext } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
 import Profile from "../components/Profile";
+import axios from "axios";
+import { Container } from "../styles/ChatStyles";
+import { SERVER_ADDRESS, SERVER_PORT } from '@env';
+import { userContext } from "../contexts/userContext";
+import { IUser } from "../util/Types";
 
 const ProfileRoute = () => {
-  return <View>{/* <Profile /> */}</View>;
-};
+  const [user, setUser] = useState<IUser>(null);
+  const [isLoading, setLoading] = useState(true);
+  const {state} = useContext(userContext);
+  
+const getUser = async() =>{
+ await axios
+  .get(`${SERVER_ADDRESS}:${SERVER_PORT}/user/getuser?userId=${state.user._id}`,
+ { headers: { "Content-Type": "application/json" },
+})
+  .then((res) => {
+      setUser(res.data);
+      setLoading(false);
+      console.log(user);
+  })
+  .catch((err) => {
+      console.log("Error");
+      console.log(err);
+  });
+  };
+ useEffect(()=> {
+   getUser();
+  }, []);
 
+  return (
+    <Container>
+
+  {isLoading ? <ActivityIndicator /> : (
+    <View>{< Profile user={user[0]} />}</View>
+    )}
+  
+  </Container>
+  )};
 export default ProfileRoute;
