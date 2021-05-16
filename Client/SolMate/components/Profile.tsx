b2ecbcc79423aff303437391929895c0a223a24c
Shiri Mesika <shirimes@gmail.com>
2021-05-16 22:06:18 +0300

    fix: show media and artists

--- Client/SolMate/components/Profile.tsx
+++ Client/SolMate/components/Profile.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useContext } from "react";
-import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
 import {
   Paragraph,
   Title,
@@ -7,11 +15,13 @@ import {
   Button,
   IconButton,
   BottomNavigation,
+  Dialog,
+  Card,
 } from "react-native-paper";
 import { userContext } from "../contexts/userContext";
 import { IUser } from "../util/Types";
 import { SERVER_ADDRESS, SERVER_PORT } from "@env";
-import { Card, Divider, ListItem } from "react-native-elements";
+import { Divider, ListItem } from "react-native-elements";
 
 interface ProfileProps {
   user: any;
@@ -19,18 +29,13 @@ interface ProfileProps {
 
 const Profile = (props) => {
   const [index, setIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const [image, setImage] = useState();
 
   const styles = StyleSheet.create({
     root: {
       alignItems: "center",
       alignContent: "center",
-    },
-    avatar: {
-      marginTop: 40,
-      alignSelf: "center",
-      width: 150,
-      height: 150,
-      borderRadius: 100,
     },
     title: {
       marginTop: 10,
@@ -53,62 +58,81 @@ const Profile = (props) => {
       justifyContent: "center",
       alignItems: "center",
       flexWrap: "wrap",
-      //paddingTop: 50,
     },
     image: {
       width: 180,
       height: 180,
     },
-    Title: {
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
       alignItems: "center",
-      alignContent: "center",
-      color: "white",
-      textAlign: "center",
-      alignSelf: "center",
-      fontSize: 24,
-      fontFamily: "Poppins_500Medium_Italic",
-    },
-    artistImage: {
-      margin: 30,
       shadowColor: "#000",
       shadowOffset: {
         width: 0,
-        height: 5,
+        height: 2,
       },
-      shadowOpacity: 0.34,
-      shadowRadius: 6.27,
-      elevation: 10,
-      //
-
-      // marginTop: 40,
-      // alignSelf: "center",
-      // width: 120,
-      // height: 120,
-      // borderRadius: 140,
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
     },
-    artist: {
-      // display: "flex",
-      // flexDirection: "row",
-      // alignItems: "center",
-      // color: "#cbc0d3",
-      // backgroundColor: "#cbc0d3",
+    card: {
+      alignItems: "center",
+      alignContent: "center",
+      width: 350,
+      margin: 15,
+      borderRadius: 20,
+      paddingTop: 15,
+      position: "absolute",
+      top: "20%",
+      bottom: "12%",
+      left: "4%",
+      right: "3%",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    cardCover: {
+      width: 340,
+      height: 430,
+      borderRadius: 20,
+    },
+    button: {
+      width: 200,
+      backgroundColor: "#8860D0",
+      fontFamily: "Poppins_300Light",
     },
   });
 
   const renderArtists = () => {
     const artistsDOM = [];
-    for (let i = 0; i < props.user.Artists.length; i++) {
+    if (props.user.Artists.length === 0) {
       artistsDOM.push(
-        <ListItem key={i} bottomDivider style={{ width: 380, height: 130 }}>
-          <Avatar.Image
-            source={{ uri: props.user.Artists[i].images[0].url }}
-            size={100}
-          />
-          <ListItem.Content>
-            <ListItem.Title>{props.user.Artists[i].name}</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
+        <Paragraph key={0} style={styles.description}>
+          No artists found ☹
+        </Paragraph>
       );
+    } else {
+      for (let i = 0; i < props.user.Artists.length; i++) {
+        artistsDOM.push(
+          <ListItem key={i} bottomDivider style={{ width: 380, height: 120 }}>
+            <Avatar.Image
+              source={{ uri: props.user.Artists[i].images[0].url }}
+              size={80}
+            />
+            <ListItem.Content>
+              <ListItem.Title>{props.user.Artists[i].name}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
+        );
+      }
     }
     //}
     return artistsDOM;
@@ -120,15 +144,32 @@ const Profile = (props) => {
     for (let i = 0; i < props.user.user.Media.length; i++) {
       mediaDOM.push(
         <View key={"media" + i}>
-          <Image
-            style={styles.image}
-            source={{ uri: props.user.user.Media[i] }}
-          />
+          <TouchableOpacity
+            onPress={() => onImagePress(props.user.user.Media[i])}
+          >
+            <Image
+              style={styles.image}
+              source={{
+                uri: `${SERVER_ADDRESS}:${SERVER_PORT}/static/${props.user.user.Media[i]}`,
+              }}
+            />
+          </TouchableOpacity>
         </View>
       );
     }
 
     return mediaDOM;
+  };
+
+  const calcAge = (date: Date) => {
+    return new Number(
+      (new Date().getTime() - new Date(date).getTime()) / 31536000000
+    ).toFixed(0);
+  };
+
+  const onImagePress = (i) => {
+    setIsVisible(true);
+    setImage(i);
   };
 
   let content = null;
@@ -137,30 +178,61 @@ const Profile = (props) => {
 
   return (
     <View style={styles.root}>
+      <Modal visible={isVisible} transparent={true}>
+        <Card style={styles.card}>
+          <Card.Cover
+            style={styles.cardCover}
+            source={{
+              uri: `${SERVER_ADDRESS}:${SERVER_PORT}/static/${image}`,
+            }}
+          ></Card.Cover>
+          <Card.Actions>
+            <Button
+              onPress={() => {
+                setIsVisible(false);
+              }}
+            >
+              Close
+            </Button>
+          </Card.Actions>
+        </Card>
+      </Modal>
       <Avatar.Image
-        size={180}
+        size={120}
         source={{
           uri: `${SERVER_ADDRESS}:${SERVER_PORT}/static/${props.user.user.picture}`,
         }}
       />
       <Title style={styles.title}>
-        {props.user.user.firstName + " " + props.user.user.lastName}
+        {props.user.user.firstName +
+          " " +
+          props.user.user.lastName +
+          ", " +
+          calcAge(props.user.user.birthday)}
       </Title>
       <Paragraph style={styles.description}>
         {props.user.user.description}
       </Paragraph>
 
       <View style={styles.tabs}>
-        <Button mode='contained' onPress={() => setIndex(0)}>
+        <Button
+          style={styles.button}
+          mode='contained'
+          onPress={() => setIndex(0)}
+        >
           Top Artists
         </Button>
-        <Button mode='contained' onPress={() => setIndex(1)}>
+        <Button
+          style={styles.button}
+          mode='contained'
+          onPress={() => setIndex(1)}
+        >
           Media
         </Button>
       </View>
 
       <ScrollView>
-        <View style={{ flexDirection: "column" }}>{content}</View>
+        <View style={styles.content}>{content}</View>
       </ScrollView>
     </View>
   );
